9a08c685e42fa8561a9e75470c557076fdcfc44d
VUHOANGTHE2610 <hoangthevu8910@gmail.com>
2024-05-03 15:03:58 +0700

    TNXD

---
+++ config.js
@@ -0,0 +1,42 @@
+var config = {
+    texts: [
+        "Biết gì không ?",   
+        "Hôm ni là ngày ra đời của ai đó á", 
+        "Bạn ni rất xinh",
+        "Siêu đáng iu",
+        "Rất kiên trì",
+        "Vì là cung kim Ngưu nên hơi cứng đầu",
+        "Nhưng mà tốt bụng",
+        "Ai cho cười mà cười :^",
+        "Biết đang nói về ai rồi chứ gì -_-",
+        "Đúng á, em đang miêu tả về chị đó",
+        "Chị rất là 素晴らしい",
+        "Chúc chị càng ngày càng xinh hơn",
+        "Hạnh phúc hơn",
+        "Đạt được nhiều thành công trong công việc",
+        "Sinh nhật vui vẻ nha nhaaaaa ^-^",
+    ],
+    /**
+     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+     * 例如
+     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     *
+     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+     */
+    imgs: {
+        "Sinh nhật vui vẻ nha nhaaaaa ^-^": "./imgs/ThaoNguyen.png",
+    },
+
+    desc: {
+        turn_on: "Tối quá nhỉ? nhấn vào đây để bật đèn nè :^",
+        play: "Ấn vào đây nè",
+        bannar_coming: "nhận tiếp vào đây nè ",
+        balloons_flying: "nhâp tiếp sẻ thấy điều thú vị á",
+        cake_fadein: "Ụa thiếu thứ gì vậy nè, hình như là...？",
+        light_candle: "Thắp nến thôi nào？",
+        wish_message: "Chúc mừng sinh nhật nhaaa, ấn tiếp và chờ xíu nha",
+        story: "Bạn có một lời nhắn ở đây nè :))",
+    }
+};
